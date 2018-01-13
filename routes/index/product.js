5b2f739a4795034d58eb919243885cf7af541451
minerchow <minerchow@126.com>
2018-01-13 20:11:37 +0800

    ajax返回

--- routes/index/product.js
+++ routes/index/product.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/product', function(req, res, next) {
+    var array = [1,2,3,4];
+    var array2 = [];
     res.render('index', { title: 'Express' });
 });
+
+router.get('/list',function(req,res,next){
+    var list = {"list":[
+        {
+            'color':"red",
+            'prize':'200'
+        },
+        {
+            'color':"green",
+            'prize':'200'
+        }
+    ]}
+    return res.json(list)
+})
 
 
 
